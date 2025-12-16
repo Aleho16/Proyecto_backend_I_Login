e6bb1f655496499205b37b86d73627b50b543028
Aleho16 <perez.alejo.rojas@gmail.com>
2025-12-15 21:50:08 -0500

    proyecto base

---
+++ Proyecto_Backend_I/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
