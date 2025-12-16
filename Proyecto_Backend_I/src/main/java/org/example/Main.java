3eaac011629bc7c68efae0cea7cf13c8a241f296
Aleho16 <perez.alejo.rojas@gmail.com>
2025-12-15 22:04:36 -0500

    Menús y flujo principal

--- Proyecto_Backend_I/src/main/java/org/example/Main.java
+++ Proyecto_Backend_I/src/main/java/org/example/Main.java
@@ -7,6 +7,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        int opcion;
 
+
+        System.out.println("1. Registrar\n2. Login");
+        opcion = sc.nextInt();
+        sc.nextLine();
+
+
+        if (opcion == 1) {
+            System.out.print("Usuario: ");
+            String u = sc.nextLine();
+            System.out.print("Password: ");
+            String p = sc.nextLine();
+            UsuarioCrud.registrar(new Usuario(u, p));
+        } else {
+            System.out.print("Usuario: ");
+            String u = sc.nextLine();
+            System.out.print("Password: ");
+            String p = sc.nextLine();
+
+
+            ResultSet rs = UsuarioCrud.login(u);
+            if (rs.next() && rs.getBoolean("activo") && rs.getString("password").equals(p)) {
+                System.out.println("Login correcto");
+                ProductoCrud.listar();
+            } else {
+                System.out.println("Login incorrecto");
+            }
+        }
     }
 }
